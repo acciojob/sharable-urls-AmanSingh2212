2b9658c4e8f88bd5b63b8dbcd5d40440896814a0
Aman Singh <112865635+AmanSingh2212@users.noreply.github.com>
2024-09-23 17:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+let form = document.getElementById('loginForm');
+let urlData = document.getElementById('urlData');
+
+form.addEventListener("submit", (e) => {
+
+	e.preventDefault();
+
+	let name = form.Name.value;
+	let year = form.Year.value;
+
+	console.log(name);
+
+	if(name == "")
+      urlData.innerText = `https://localhost:8080/?name=${name}`;
+	
+})
